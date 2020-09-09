f97eaa50077a02b6aee394c6240d60dbd99a1db5
github username <github email address>
2020-09-09 17:57:42 -0500

    Added ability to check if username exist

--- lib/app.rb
+++ lib/app.rb
@@ -5,39 +5,81 @@ def greet
     puts 'Welcome to the App, the best source for festival information!'
     account = $prompt.yes?("Do you have an account?")
     if account == true
-        fan_festival
+        name = $prompt.ask("Enter account name:")
+        find_fan(name)
     else
         create_fan
     end
 end
 
-def fan_festival
+def find_fan(name)
+    if Fan.find_by(name: name) == nil
+        puts "Sorry User not found"
+        create_fan
+    else
+        fan = Fan.find_by(name: name)
+        fan_festival(fan)
+    end
+    # fan_festival(fan)
+end
+
+def fan_festival(fan)
+    # Finds festival
     upcoming_festival = $prompt.select("Choose an upcoming festival", %w(EDC Audiotistic Madness))
     found_festival = Festival.find_by(name: upcoming_festival)
+
     puts "#{found_festival.name} is #{found_festival.date}. This year in #{found_festival.location} you can catch #{found_festival.artist}"
     puts "Get your #{found_festival.name} ticket for $#{found_festival.price} before they sellout!"
-    buy_ticket = $prompt.yes?("Do you want to buy a festival ticket?")
+    #Buys ticket for current user
+    buy_ticket = $prompt.yes?("Do you want to buy a festival ticket for #{found_festival.name}. The total is $#{found_festival.price}")
     if buy_ticket == true
-        name = $prompt.ask("Enter your name:")
-        t_name = Fan.find_by(name: name)
-        Ticket.create(name: "#{found_festival.name}", fan_id: t_name.id, festival_id: found_festival.id)
+        Ticket.create(name: "#{found_festival.name}", fan_id: fan.id, festival_id: found_festival.id)
         puts "Purchase Successful"
-        binding.pry
+        
     else
-        return
+        multi_option(fan)
     end
 end
 
 def create_fan
-    puts 'Enter your name to continue'
-    name = $prompt.ask("What is your name?")
-    Fan.create(name: name)
-    fan_festival
+    already_have = $prompt.yes?("Are you sure you have an account?")
+    if already_have == true
+        name = $prompt.ask("Enter account name so we can check:")
+        find_fan(name)
+    else
+        puts 'Enter your name to continue'
+        name = $prompt.ask("What is your name?")
+        Fan.create(name: name)
+        find_fan(name)
+    end
 end
 
-def purchase_ticket
+def multi_option(fan)
+    user_pick = $prompt.select("Do you need ticket help?", %w(Refund Update Exit))
+    if user_pick == "Refund"
+        refund_ticket(fan)
+    elsif user_pick == "Update"
+        update_ticket(fan)
+    else
+        puts "Thank you for using our app!"
+    end
+end
+
+def refund_ticket(fan)
+    all_tix = Fan.find_by(name: fan.name).tickets
+    # tix = $prompt.select
+    tix = all_tix.each do |ticket|
+        
+    end
+   
+    
+end
+
+def update_ticket(fan)
 
 end
+
+
 
 
 
--- lib/models/festival.rb
+++ lib/models/festival.rb
@@ -1,4 +1,5 @@
 class Festival < ActiveRecord::Base
     has_many :tickets
     has_many :fans, through: :tickets
+    
 end
